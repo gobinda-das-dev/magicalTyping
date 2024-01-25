7891413bf24e33f2ec043e34c16a28d9a0f68d61
Gobinda Das <144037976+gobinda-das-dev@users.noreply.github.com>
2024-01-25 11:57:09 +0530

    add basic files

---
+++ script.js
@@ -0,0 +1,65 @@
+document.addEventListener('keydown', function (event) {
+    let x = Math.floor(Math.random() * 100);
+    let y = Math.floor(Math.random() * 100);
+
+    let xPositionIn = Math.floor((Math.random() * 80) - 20)
+    let yPositionIn = Math.floor((Math.random() * 80) - 20)
+    let xPositionOut = Math.floor((Math.random() * 80) - 20)
+    let yPositionOut = Math.floor((Math.random() * 80) - 20)
+
+    if (
+        (event.key.length === 1 && event.key.match(/[a-zA-Z]/)) ||
+        (event.key.length === 1 && event.key.match(/[0-9]/))
+    ) {
+        var span = document.createElement('span');
+        span.textContent = event.key;
+
+        span.style.top = x + '%';
+        span.style.left = y + '%';
+
+        // Create a new timeline for each span
+        let tl = gsap.timeline();
+        tl
+            .from(span, {
+                x: xPositionIn,
+                y: yPositionIn,
+                rotate: xPositionIn + yPositionIn,
+                opacity: 0,
+            })
+            .to(span, {
+                x: xPositionOut,
+                y: yPositionOut,
+                rotate: xPositionIn + xPositionOut,
+                opacity: 0,
+                delay: .5,
+            });
+
+        document.body.appendChild(span);
+    }
+});
+setTimeout(() => {
+    let h5 = document.querySelector("h5");
+    gsap.timeline()
+        .to("h5", {
+            top: "100%",
+            onComplete: () => {
+                h5.remove()
+            }
+        })
+}, 2000);
+
+Shery.mouseFollower();
+Shery.makeMagnet("svg text");
+
+document.querySelector("body").onmouseenter = () => {
+    gsap.to(".mousefollower", {
+        height: 14,
+        width: 14,
+    })
+}
+document.querySelector("body").onmouseleave = () => {
+    gsap.to(".mousefollower", {
+        height: 0,
+        width: 0,
+    })
+}
